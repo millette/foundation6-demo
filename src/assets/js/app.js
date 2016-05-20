1cd746e72babfa1c174a28d9d0fec09de2f16dbe
Robin Millette <robin@millette.info>
2016-05-19 22:37:46 -0400

    Readjust grid heights after vega renders.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,61 +1,95 @@
-$(() => {
-  'use strict'
+'use strict'
 
+$(() => {
   const wow = new WOW()
+
   wow.init()
   $(document).foundation()
 
-  console.log('fetching!')
-  fetch('assets/repos.json')
-    .then(function(response) {
-      return response.json()
-    }).then(function(json) {
-      console.log('parsed json', json.length)
-    }).catch(function(ex) {
-      console.log('parsing failed', ex)
-    })
+  var initalized = false
 
-  const embed_spec = {
-    mode: 'vega-lite',
-    actions: false,
-    background: 'white',
-    renderer: 'svg',
-    spec: {
-      "description": "A simple bar chart with embedded data.",
-      "data": {
-        "values": [
-          {"a": "A","b": 28}, {"a": "B","b": 55}, {"a": "C","b": 43},
-          {"a": "D","b": 91}, {"a": "E","b": 81}, {"a": "F","b": 53},
-          {"a": "G","b": 19}, {"a": "H","b": 87}, {"a": "I","b": 52}
-        ]
-      },
-      "mark": "bar",
-      "encoding": {
-        "x": {"field": "a", "type": "ordinal"},
-        "y": {"field": "b", "type": "quantitative"}
-      }
+  const vegaEmbed = (spec, el) => {
+    const embed_spec = {
+      mode: 'vega-lite',
+      actions: false,
+      renderer: 'svg',
+      spec: spec
     }
+
+    const vegaEl = document.createElement('div')
+    vg.embed(vegaEl, embed_spec, (err, result) => {
+      const elEl = document.querySelector(el)
+      if (err) {
+        if (elEl) {
+          const preEl = document.createElement('pre')
+          preEl.innerHTML = err
+          elEl.appendChild(preEl)
+        }
+        return
+      }
+      elEl.appendChild(vegaEl)
+      const svg = result.view._el.firstChild
+      const width = svg.getAttribute('width')
+      const height = svg.getAttribute('height')
+      svg.setAttribute('viewBox', `0 0 ${width} ${height}`)
+      svg.setAttribute('width', '100%')
+      svg.setAttribute('height', '100%')
+      if (!initalized) {
+        initalized = true
+        Foundation.reInit($('#grid'));
+      }
+    })
   }
 
-  // vg.embed('#viz', embed_spec, function(error, result) {})
-
-  const vegaEl = document.createElement('div')
-  vg.embed(vegaEl, embed_spec, (err, result) => {
-    const elEl = document.querySelector('#viz')
-    if (err) {
-      if (elEl) {
-        const preEl = document.createElement('pre')
-        preEl.innerHTML = err
-        elEl.appendChild(preEl)
+  fetch('assets/repos.json')
+    .then((response) => response.json())
+    .then((json) => {
+      const spec = {
+        description: 'A simple bar chart with embedded data.',
+        data: {
+          values: json
+        },
+        transform: {filter: 'datum.public_repos > 20'},
+        mark: 'bar',
+        encoding: {
+          x: {
+            field: 'public_repos',
+            bin: { maxbins: 40 },
+            type: 'quantitative'
+            // type: 'ordinal'
+          },
+          y: {
+            aggregate: 'count', field: '*', type: 'quantitative'
+          }
+        }
       }
-      return
-    }
-    elEl.appendChild(vegaEl)
-    const svg = result.view._el.firstChild
-    const width = svg.getAttribute('width')
-    const height = svg.getAttribute('height')
-    svg.setAttribute('viewBox', `0 0 ${width} ${height}`)
-    svg.setAttribute('width', '100%')
-    svg.setAttribute('height', '100%')
-  })
+
+      const spec2 = {
+        description: 'A simple bar chart with embedded data.',
+        data: {
+          values: json
+        },
+        transform: {filter: 'datum.members > 5'},
+        mark: 'bar',
+        encoding: {
+          x: {
+            field: 'members',
+            // bin: { maxbins: 20 },
+            // type: 'quantitative'
+            type: 'ordinal'
+          },
+          y: {
+            aggregate: 'count',
+            field: '*',
+            type: 'quantitative'
+          }
+        }
+      }
+
+      vegaEmbed(spec, '#viz')
+      vegaEmbed(spec2, '#viz2')
+    })
+    .catch(function(ex) {
+      console.log('parsing failed', ex)
+    })
 })
